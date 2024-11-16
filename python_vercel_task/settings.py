cdd64b17b3543281bc7c149094656719644e0e83
rubyssj <ggruben3344@gmail.com>
2024-11-16 11:41:44 -0300

    settings.py

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY_env'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
